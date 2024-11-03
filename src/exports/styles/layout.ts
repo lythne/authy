55322a2748d4d7a884cbb9c0e896d91583ef687d
lythne <lythne@gmail.com>
2024-11-03 14:41:05 +0300

    Exported Layout Styles - Exported `Header.module.css`, `Container.module.css`, `Main.module.css` and `Footer.module.css`

---
+++ src/exports/styles/layout.ts
@@ -0,0 +1,4 @@
+export { default as HeaderStyles } from '@/styles/ui/layout/Header/Header.module.css';
+export { default as ContainerStyles } from '@/styles/ui/layout/Container/Container.module.css';
+export { default as MainStyles } from '@/styles/ui/layout/Main/Main.module.css';
+export { default as FooterStyles } from '@/styles/ui/layout/Footer/Footer.module.css';
