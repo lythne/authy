55867eb12cd639743cc2ce73d57bf911a9a82669
lythne <lythne@gmail.com>
2024-11-03 14:14:04 +0300

    Exported Layout Components - Exported `Header`, `Container`, `Main` and `Footer` components

---
+++ src/exports/ui/layout.ts
@@ -0,0 +1,4 @@
+export { default as Header } from '@/ui/layout/Header/Header';
+export { default as Container } from '@/ui/layout/Container/Container';
+export { default as Main } from '@/ui/layout/Main/Main';
+export { default as Footer } from '@/ui/layout/Footer/Footer';
