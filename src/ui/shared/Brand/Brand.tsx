1c2be76769c47301030c5ed23728bde3fc8064de
lythne <lythne@gmail.com>
2024-11-03 15:25:41 +0300

    Created `Logo` & `Brand` Components

---
+++ src/ui/shared/Brand/Brand.tsx
@@ -0,0 +1,10 @@
+import Logo from '../Logo/Logo';
+
+export default function Brand() {
+    return (
+        <div>
+            <Logo />
+            <p>Authy</p>
+        </div>
+    );
+}
---
+++ src/ui/shared/Logo/Logo.tsx
@@ -0,0 +1,12 @@
+import Image from 'next/image';
+
+export default function Logo() {
+    return (
+        <Image
+            src={'/logo.svg'}
+            alt="Logo"
+            width={32}
+            height={32}
+        />
+    );
+}
