2349bed198ff72c4a4af4f100b5af3ff6561c3b2
lythne <lythne@gmail.com>
2024-11-03 14:16:35 +0300

    Mounted Layout Components

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,13 @@
+import { Container, Footer, Header, Main } from '@/exports/ui/layout';
+
 export default function Landing() {
-    return <></>;
+    return (
+        <>
+            <Header />
+            <Container>
+                <Main />
+                <Footer />
+            </Container>
+        </>
+    );
 }
